aa2759a34fdbe75ea0ab0a5c2c9e71c42ab6d785
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-11-07 21:59:05 +0200

    Добавляє ДЗ

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./css/styles.css";
 import countryCardTpl from "./templates/country-card.hbs";
-import API from "./js/api-service.js";
+import countriesListCardTpl from "./templates/countries-list-card.hbs";
+import API from "./js/fetchCountries.js";
 import getRefs from "./js/get-refs";
-
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
 const debounce = require("lodash.debounce");
 
 const refs = getRefs();
@@ -17,13 +20,13 @@ function onSearch(e) {
   //   const searchQuery = form.currentTarget.elements.query.value;
   const searchQuery = document.getElementsByClassName("js-form-enter")[0].value;
 
-  API.fetchCountry(searchQuery).then(renderCountryCard).catch(fetchError);
+  API.fetchCountries(searchQuery).then(auditCountriesConditions).catch(fetchError);
 }
 
 // console.log("!!!fetchCountry", fetchCountry);
 
-function renderCountryCard(country) {
-  const markup = countryCardTpl(country[0]);
+function renderCard(template, country) {
+  const markup = template(country);
   //   console.log("markup!!!!!", markup);
   //   console.log("country[0]!!!!!!!", country[0]);
   //   console.log("country!!!!!!!", country);
@@ -32,4 +35,22 @@ function renderCountryCard(country) {
 
 function fetchError(error) {
   console.log("Якась халепа!");
+}
+
+function onErrorEnterCountryTooMany() {
+  error({
+    text: "To many matches found. Please enter a more specific query!",
+    delay: 2000,
+  });
+}
+
+function auditCountriesConditions(country) {
+  if (country.length > 10) {
+    // cleanFieldSearch();
+    onErrorEnterCountryTooMany();
+  } else if (country.length > 1 && country.length < 11) {
+    renderCard(countriesListCardTpl, country);
+  } else if (country.length === 1) {
+    renderCard(countryCardTpl, country[0]);
+  }
 }
--- src/js/api-service.js
+++
@@ -1,9 +0,0 @@
-function fetchCountry(countryName) {
-  const BASE_URL = "https://restcountries.eu/rest/v2/name";
-
-  return fetch(`${BASE_URL}/${countryName}`).then((response) => {
-    return response.json();
-  });
-}
-
-export default { fetchCountry };
